d8370ca48a67772157e14efc9e9165072d37be51
OKRAH DAUDA KINGSLEY(ODK) <99811118+daudaokrahkingsley@users.noreply.github.com>
2022-09-21 12:32:14 +0000

    Add files via upload

---
+++ PrimeNumbers.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class PrimeExample4 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        // stating the range...
+        System.out.print("Enter the first number : ");
+        int start = s.nextInt();
+        System.out.print("Enter the second number : ");
+        int end = s.nextInt();
+        System.out.println("List of prime numbers between " + start + " and " + end);
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
